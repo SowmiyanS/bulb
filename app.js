99577763b616a708623543f7f1c9efb76a5063ab
SowmiyanS <sowmiyan9491@gmail.com>
2023-07-14 14:35:32 +0530

    added assets folder

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ function Change() {
     var img = document.getElementById("bulb");
     if(state == 0) {
         state = 1;
-        img.src = "https://media.geeksforgeeks.org/wp-content/uploads/ONbulb.jpg";
+        img.src = "./assets/pic_bulbon.gif";
     }
     else if(state == 1) {
         state = 0;
-        img.src = "https://media.geeksforgeeks.org/wp-content/uploads/OFFbulb.jpg";
+        img.src = "./assets/pic_bulboff.gif";
     }
     else {
         console.log("Error on change function");
